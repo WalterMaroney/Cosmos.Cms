cf41efc68807516f635e87bd22346e92d2fd64b2
toiyabe <52581997+toiyabe@users.noreply.github.com>
2022-01-12 06:03:12 -0800

    Removed unused references

--- CDT.Cosmos.Cms/Models/UserRolesViewModel.cs
+++ CDT.Cosmos.Cms/Models/UserRolesViewModel.cs
@@ -1,7 +1,13 @@
 ﻿namespace CDT.Cosmos.Cms.Models
 {
+    /// <summary>
+    /// User role view model
+    /// </summary>
     public class UserRolesViewModel
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public UserRolesViewModel()
         {
             Administrator = false;
@@ -12,17 +18,42 @@
             RemoveAccount = false;
             TeamMember = false;
         }
-
+        /// <summary>
+        /// User email address
+        /// </summary>
         public string UserEmailAddress { get; set; }
+        /// <summary>
+        /// User ID
+        /// </summary>
         public string UserId { get; set; }
+        /// <summary>
+        /// Is an Administrator?
+        /// </summary>
         public bool Administrator { get; set; }
+        /// <summary>
+        /// Is an Editor?
+        /// </summary>
         public bool Editor { get; set; }
+        /// <summary>
+        /// Is an Author?
+        /// </summary>
         public bool Author { get; set; }
+        /// <summary>
+        /// Is a Reviewer?
+        /// </summary>
         public bool Reviewer { get; set; }
+        /// <summary>
+        /// Is a member of a Team?
+        /// </summary>
         public bool TeamMember { get; set; }
+        /// <summary>
+        /// Has no role?
+        /// </summary>
         public bool NoRole { get; set; }
         public bool RemoveAccount { get; set; }
-
+        /// <summary>
+        /// User role name
+        /// </summary>
         public string UserRole { get; set; }
     }
 }
